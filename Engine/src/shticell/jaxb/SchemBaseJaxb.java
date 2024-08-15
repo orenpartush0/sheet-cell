2974e124bf26c8ac968809dc5989b42a014dd46e
Noa Altshuler <noaalts@gmail.com>
2024-08-15 17:55:41 +0300

    last by noa to merge

--- Engine/src/shticell/jaxb/SchemBaseJaxb.java
+++ Engine/src/shticell/jaxb/SchemBaseJaxb.java
@@ -10,13 +10,15 @@ import java.io.InputStream;
 public class SchemBaseJaxb {
     private final static String JAXB_XML_GAME_PACKAGE_NAME = "Engine.shticell.jaxb.schema;";
 
-    private static STLSheet deserializeFrom(InputStream in) throws JAXBException{
+    public static STLSheet deserializeFrom(InputStream in) throws JAXBException{
         JAXBContext jc = JAXBContext.newInstance(JAXB_XML_GAME_PACKAGE_NAME);
         Unmarshaller u = jc.createUnmarshaller();
         STLSheet unmarshal = (STLSheet) u.unmarshal(in);
         return unmarshal;
         
     }
+
+
     
     
 }
