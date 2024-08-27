25f06063a5234f10cc6fe91b5df06a8f526699db
oren <orenpartush0@gmail.com>
2024-08-28 02:46:03 +0300

    alot of stuff ,m

--- UI/src/userInterface/Program.java
+++
@@ -1,8 +0,0 @@
-package userInterface;
-
-public class Program {
-    public static void main(String[] args) {
-        ConsoleManager manager = new ConsoleManager();
-        manager.Run();
-    }
-}
