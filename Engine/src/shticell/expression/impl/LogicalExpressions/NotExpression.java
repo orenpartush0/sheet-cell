2d85883e05b54478d2ed904ef12a4f6b70c29cae
Noa Altshuler <noaalts@gmail.com>
2024-09-03 21:42:49 +0300

    not

---
+++ Engine/src/shticell/expression/impl/LogicalExpressions/NotExpression.java
@@ -0,0 +1,16 @@
+package shticell.expression.impl.LogicalExpressions;
+
+import shticell.expression.api.Expression;
+import shticell.sheet.cell.value.EffectiveValue;
+import shticell.sheet.cell.value.EffectiveValueImpl;
+import shticell.sheet.cell.value.ValueType;
+
+public class NotExpression implements Expression {
+    @Override
+    public EffectiveValue eval(Expression... expressions) {
+        if(expressions.length != 1){
+            throw new IllegalArgumentException("If must have 1 expression");
+        }
+        return new EffectiveValueImpl(!expressions[0].eval().getValueWithExpectation(Boolean.class), ValueType.BOOLEAN);
+    }
+}
