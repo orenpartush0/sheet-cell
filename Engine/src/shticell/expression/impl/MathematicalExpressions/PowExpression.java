19249b54453e00efe68901392372c89e45084574
Noa Altshuler <noaalts@gmail.com>
2024-09-03 21:36:26 +0300

    bigger and pow

---
+++ Engine/src/shticell/expression/impl/MathematicalExpressions/PowExpression.java
@@ -0,0 +1,20 @@
+package shticell.expression.impl.MathematicalExpressions;
+
+import shticell.expression.api.Expression;
+import shticell.sheet.cell.value.EffectiveValue;
+import shticell.sheet.cell.value.EffectiveValueImpl;
+import shticell.sheet.cell.value.ValueType;
+
+
+public class PowExpression implements Expression {
+    @Override
+    public EffectiveValue eval(Expression... expressions) {
+        if (expressions.length != 2) {
+            throw new UnsupportedOperationException("Pow needs two arguments");
+        }
+        EffectiveValue left = expressions[0].eval();
+        EffectiveValue right = expressions[1].eval();
+
+        return new EffectiveValueImpl(Math.pow(left.getValueWithExpectation(Double.class),right.getValueWithExpectation(Double.class)), ValueType.NUMERIC);
+    }
+}
