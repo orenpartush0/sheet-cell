a249725f5e5eb6ae59f4f834eec2f8c5859c93de
Noa Altshuler <noaalts@gmail.com>
2024-09-03 21:43:55 +0300

    not fix and less

---
+++ Engine/src/shticell/expression/impl/LogicalExpressions/LessExpression.java
@@ -0,0 +1,22 @@
+package shticell.expression.impl.LogicalExpressions;
+
+import shticell.expression.api.Expression;
+import shticell.sheet.cell.value.EffectiveValue;
+import shticell.sheet.cell.value.EffectiveValueImpl;
+import shticell.sheet.cell.value.ValueType;
+
+public class LessExpression implements Expression {
+    @Override
+    public EffectiveValue eval(Expression... expressions) {
+        if(expressions.length != 2){
+            throw new IllegalArgumentException("Less must have 2 expressions");
+        }
+
+        EffectiveValue leftVal = expressions[0].eval();
+        EffectiveValue rightVal = expressions[1].eval();
+
+        return new EffectiveValueImpl(leftVal.getValueWithExpectation(Double.class)  <= rightVal.getValueWithExpectation(Double.class)
+                , ValueType.BOOLEAN);
+    }
+
+}
