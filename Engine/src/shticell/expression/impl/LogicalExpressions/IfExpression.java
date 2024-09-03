5d744941406d83c01549f0d16e61eb571eaf55c5
Noa Altshuler <noaalts@gmail.com>
2024-09-03 21:40:40 +0300

    if

---
+++ Engine/src/shticell/expression/impl/LogicalExpressions/IfExpression.java
@@ -0,0 +1,26 @@
+package shticell.expression.impl.LogicalExpressions;
+
+import shticell.expression.api.Expression;
+import shticell.sheet.cell.value.EffectiveValue;
+import shticell.sheet.cell.value.EffectiveValueImpl;
+import shticell.sheet.cell.value.ValueType;
+
+public class IfExpression implements Expression {
+    @Override
+    public EffectiveValue eval(Expression... expressions) {
+        if(expressions.length != 3){
+            throw new IllegalArgumentException("If must have 2 expressions");
+        }
+
+        EffectiveValue condition = expressions[0].eval();
+
+        Boolean conditionValue = condition.getValueWithExpectation(Boolean.class);
+        if(conditionValue){
+            return expressions[1].eval();
+        }
+        else{
+            return expressions[2].eval();
+        }
+
+    }
+}
